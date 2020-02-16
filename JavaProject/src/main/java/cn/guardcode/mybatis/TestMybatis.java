e31e2f01aaeb1c88e5cbd2e7d8d8dcaaecf8ae23
gd <1941796841@qq.com>
2020-02-16 13:18:51 +0800

    ff

--- JavaProject/src/main/java/cn/guardcode/mybatis/TestMybatis.java
+++ JavaProject/src/main/java/cn/guardcode/mybatis/TestMybatis.java
@@ -1,22 +1,33 @@
 package cn.guardcode.mybatis;
 
+//import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 
 public class TestMybatis {
     public static void main(String[] args) {
-        //String resource = "cn/guardcode/mybatis/mybatis-config.xml";
+//        String path = "cn/guardcode/mybatis/mybatis-config.xml";
+        String path = "D:\\Project\\cn\\guardcode\\mybatis\\mybatis-config.xml";
+        File file = new File(path);
+        String absolutePath = file.getAbsolutePath();
+        InputStream inputStream1 = null;
+        try {
+            inputStream1 = new FileInputStream(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        System.out.println(absolutePath);
         String resource = "D:\\Project\\JavaProject\\src\\main\\java\\cn\\guardcode\\mybatis\\mybatis-config.xml";
         InputStream inputStream = null;
-        try {
+        /*try {
             inputStream = Resources.getResourceAsStream(resource);
         } catch (IOException e) {
             e.printStackTrace();
-        }
-        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        }*/
+        SqlSessionFactory sqlSessionFactory
+        = new SqlSessionFactoryBuilder().build(inputStream1);
     }
 }
